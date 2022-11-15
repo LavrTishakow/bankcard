06e58374061f190f921f12089a4ede99ac701d49
Lavr Tishakow <drakonavitshye@gmail.com>
2022-11-15 20:53:15 +0300

    1

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const expirationSelect = document.querySelector("[data-expiration-year]")
+
+const currentYear = Date().getFullYear()
+for (let i = currentYear; i < currentYear + 10; i++) {
+	const option = document.createElement("option")
+
+}
