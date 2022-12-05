7e9725570ff91cfe7034f81e66d6d159c3affcf4
Lavr Tishakov <tishakowlavrentiy@gmail.com>
2022-12-06 01:05:22 +0300

    1

--- script.js
+++ script.js
@@ -3,5 +3,29 @@ const expirationSelect = document.querySelector("[data-expiration-year]")
 const currentYear = new Date().getFullYear()
 for (let i = currentYear; i < currentYear + 10; i++) {
 	const option = document.createElement("option")
+	option.value = i
+	option.innerText = i
+	expirationSelect.append(option)
+}
 
+
+document.addEventListener("keydown", e => {
+	const input = e.target
+	const key = e.key
+	if (!isConnectedInput(input)) return
+
+	switch (key) {
+		case "ArrowLeft": {
+			if (input.selectionStart === 0 && input.selectionEnd === 0)
+		}
+
+		default: {
+
+		}
+	}
+})
+
+function isConnectedInput(input) {
+	const parent = input.closest("[data-connected-inputs]")
+	return target.matches("input") && parent != null
 }
